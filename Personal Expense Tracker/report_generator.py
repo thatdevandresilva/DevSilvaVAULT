de4e841531e8eaf28c4157e2347fcbb175524b50
DevAndreSilva <andresilva.programming@gmail.com>
2024-10-17 21:52:48 +0100

    New Feature

---
+++ Personal Expense Tracker/report_generator.py
@@ -0,0 +1,23 @@
+# date filtering: user can especify the range of the report (weekly or monthly) or start and end date
+# only expenses within those dates can be printed
+# sumarize the expenses per category
+# sumarize the overall expenses within the range
+# make the option to save the report as an csv or view it directly on the terminal
+# csv should include: date of expense, category and amount
+
+from datetime import date, datetime
+import expense_tracker
+
+def report_generator():
+    # input
+
+
+    # processing
+
+
+    # output
+
+
+    pass
+
+report_generator()
